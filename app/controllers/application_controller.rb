09dfd78382f47885f7d2923d0140a3b6d130ac5d
KyRZy <krzysiekk09@o2.pl>
2017-12-04 15:15:41 +0100

    Fixed swiss system brackets to be responsive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_filter :flash_to_headers
+  after_action :flash_to_headers
   
   protected
   
